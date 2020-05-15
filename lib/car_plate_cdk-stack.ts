7da7cac1ba20d0c79a528b81f40ded54afbb2f1f
Himanshu Goyal <h.goyal@alumni.ubc.ca>
2020-05-15 00:07:12 -0700

    test if everything is committed

--- lib/car_plate_cdk-stack.ts
+++ lib/car_plate_cdk-stack.ts
@@ -1,9 +1,15 @@
 import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda';
 
 export class CarPlateCdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    let car_plate_lambda = new lambda.Function(this, 'car_plate_recog', {
+      functionName: "car_plate_recog_cdk",
+      runtime: lambda.Runtime.NODEJS_10_X,
+      code: lambda.Code.fromAsset("lambda"),
+      handler: 'hello.handler'
+    })
   }
 }
